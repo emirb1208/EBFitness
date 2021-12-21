f803a5597a5dcf12bfae315297881aa936bd7818
emirb1208 <emir.beba@stu.ibu.edu.ba>
2021-12-21 20:57:38 +0100

    DAO

---
+++ api/dao/BaseDao.class.php
@@ -0,0 +1,26 @@
+<<?php
+
+class BaseDao {
+
+public function __construct() {
+
+}
+
+public function insert() {
+
+}
+
+public function update() {
+  
+}
+
+public function query() {
+
+}
+
+public function query_unique(){
+
+}
+
+}
+ ?>
