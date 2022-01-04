85b2edffff4001d07ec62f9fa2b5b7a384000029
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-04 13:02:11 +0100

    reorganize routes.

--- api/index.php
+++ api/index.php
@@ -3,35 +3,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require dirname(__FILE__).'/../vendor/autoload.php';
-require dirname(__FILE__)."/dao/AccountDao.class.php";
-require dirname(__FILE__)."/dao/UserDao.class.php";
-require dirname(__FILE__)."/dao/InstructorDao.class.php";
-require dirname(__FILE__)."/dao/FitnessGoalDao.class.php";
-require dirname(__FILE__)."/dao/WorkoutPlanDao.class.php";
+require_once dirname(__FILE__).'/../vendor/autoload.php';
+require_once dirname(__FILE__)."/dao/AccountDao.class.php";
+require_once dirname(__FILE__)."/dao/UserDao.class.php";
+require_once dirname(__FILE__)."/dao/InstructorDao.class.php";
+require_once dirname(__FILE__)."/dao/FitnessGoalDao.class.php";
+require_once dirname(__FILE__)."/dao/WorkoutPlanDao.class.php";
+
 
 Flight::register('accountDao', 'AccountDao');
 
-Flight::route('GET /accounts', function(){
-    Flight::json(Flight::accountDao()->get_all(0,10));
-});
-
-Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountDao()->get_by_id($id));
-});
-
-Flight::route('POST /accounts', function(){
-    $request = Flight::request()->data->getData();
-    Flight::json($Flight::accountDao()->add($data));
-});
-
-Flight::route('PUT /accounts/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::accountDao()->update($id, $data);
-    $account = Flight::accountDao()->get_by_id($id);
-    Flight::json($account);
-});
+require_once dirname(__FILE__)."/routes/accounts.php";
 
 
 Flight::start();
