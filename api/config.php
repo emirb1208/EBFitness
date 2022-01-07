dc0f901bf62db5863e4cad1916ff475545573204
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-07 14:38:12 +0100

    changing password for database connection

--- api/config.php
+++ api/config.php
@@ -1,8 +1,10 @@
 <?php
 class Config {
+
+  const DATE_FORMAT = "Y-m-d H:i:s";
   const DB_HOST = "localhost";
-  const DB_USERNAME = "root";
-  const DB_PASSWORD = "";
+  const DB_USERNAME = "emirbeba12";
+  const DB_PASSWORD = "emiremir";
   const DB_SCHEME = "ebfitness";
 }
 
