f84e6616d582bc0e52b784370d63a4f0684c5d79
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-12 11:09:53 +0100

    fixing FitnessGoalDao

--- api/dao/FitnessGoalDao.class.php
+++ api/dao/FitnessGoalDao.class.php
@@ -1,7 +1,7 @@
 <?php
 require_once dirname(__FILE__)."/BaseDao.class.php";
 
-class FitnessDao extends BaseDao {
+class FitnessGoalDao extends BaseDao {
 
   public function __construct(){
     parent::__construct("fitness_goals");
