6dc46337d595f9ce2152aa45dca3907aac02203d
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-13 13:22:33 +0100

    add order by feature

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -11,6 +11,21 @@ require_once dirname(__FILE__)."/../config.php";
 class BaseDao {
 protected $connection;
 
+private $table;
+
+public function parse_order($order){
+  switch(substr($order, 0, 1)){
+    case '-': $order_direction = "ASC"; break;
+    case '+': $order_direction = "DESC"; break;
+    default: throw new Exception("Invalid order format. First character should be either + or -"); break;
+  };
+
+  // Filter SQL injection attacks on column name
+  $order_column = trim($this->connection->quote(substr($order, 1)),"'");
+
+  return [$order_column, $order_direction];
+}
+
 public function __construct($table) {
   $this->table = $table;
   try {
@@ -75,8 +90,13 @@ public function get_by_id($id){
   return $this->query_unique("SELECT * FROM ".$this->table." WHERE id = :id", ["id" => $id]);
 }
 
-public function get_all($offset = 0, $limit = 25){
-  return $this->query("SELECT * FROM ".$this->table." LIMIT ${limit} OFFSET ${offset}", []);
+public function get_all($offset = 0, $limit = 25, $order="-id"){
+  list($order_column, $order_direction) = self::parse_order($order);
+
+  return $this->query("SELECT *
+                       FROM ".$this->table."
+                       ORDER BY ${order_column} ${order_direction}
+                       LIMIT ${limit} OFFSET ${offset}", []);
 }
 
 }
