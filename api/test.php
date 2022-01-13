3717d1a1ef0d3f90cca6af0a2c953c9c15ac00b5
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-13 10:46:41 +0100

    test.php fixing

--- api/test.php
+++ api/test.php
@@ -7,15 +7,7 @@ require_once dirname(__FILE__)."/dao/WorkoutPlanDao.class.php";
 
 $dao = new UserDao();
 
-/*for ($i = 0; $i < 50; $i++){
-  $dao->add([
-    "name" => base64_encode(random_bytes(10)),
-    "description" => "Some description"
-  ]);
-}
-*/
+$users = $dao->get_all($_GET['offset'], $_GET['limit']);
 
-$workoutplans = $dao->get_all($_GET['offset'], $_GET['limit']);
-//print_r($workoutplans);
-echo json_encode($workoutplans, JSON_PRETTY_PRINT);
+echo json_encode($users, JSON_PRETTY_PRINT);
 ?>
