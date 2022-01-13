62e47a81a7be3d704d4012047ba48479e0fa0f7a
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-13 09:54:09 +0100

    reorganize accounts.php

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -1,25 +1,30 @@
 <?php
 
 Flight::route('GET /accounts', function(){
-    $offset = Flight::query('offset', 0);
-    $limit = Flight::query('limit', 10);
-    $search = Flight::query('search');
-
-    Flight::json(Flight::accountService()->get_accounts($search, $offset, $limit));
-});
+        Flight::json(Flight::accountDao()->get_all(0,10));
+        /*$offset = Flight::query('offset', 0);
+        $limit = Flight::query('limit', 10);
+        $search = Flight::query('search');
+        Flight::json(Flight::accountService()->get_accounts($search, $offset, $limit));
+        */
+    });
 
 Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountService()->get_by_id($id));
+    Flight::json(Flight::accountDao()->get_by_id($id));
 });
 
 Flight::route('POST /accounts', function(){
     $data = Flight::request()->data->getData();
-    Flight::json(Flight::accountService()->add($data));
+    Flight::json(Flight::accountDao()->add($data));
+    //Flight::json(Flight::accountService()->add($data));
 });
 
 Flight::route('PUT /accounts/@id', function($id){
-    $data = Flight::request()->data->getData();
-    Flight::json(Flight::accountService()->update($id, $data));
+    $request = Flight::request();
+    $data = $request->data->getData();
+    Flight::accountDao()->update($id, $data);
+    Flight::json(Flight::accountDao()->get_by_id($id));
+    //Flight::json(Flight::accountService()->update($id, $data));
 });
 
 
