15585f205baacd0f29e9bcb4932dad274d13e4cf
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-13 09:54:35 +0100

    reorganize routes.

--- api/index.php
+++ api/index.php
@@ -11,33 +11,7 @@ require_once dirname(__FILE__)."/dao/UserDao.class.php";
 
 Flight::register('accountDao', 'AccountDao');
 
-Flight::route('GET /accounts', function(){
-    Flight::json(Flight::accountDao()->get_all(0,10));
-    /*$offset = Flight::query('offset', 0);
-    $limit = Flight::query('limit', 10);
-    $search = Flight::query('search');
-    Flight::json(Flight::accountService()->get_accounts($search, $offset, $limit));
-    */
-});
-
-Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountDao()->get_by_id($id));
-});
-
-
-Flight::route('POST /accounts', function(){
-    $data = Flight::request()->data->getData();
-    Flight::json(Flight::accountDao()->add($data));
-    //Flight::json(Flight::accountService()->add($data));
-});
-
-Flight::route('PUT /accounts/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::accountDao()->update($id, $data);
-    Flight::json(Flight::accountDao()->get_by_id($id));
-    //Flight::json(Flight::accountService()->update($id, $data));
-});
+require_once dirname(__FILE__)."/routes/accounts.php";
 
 /* Utility function for reading query parameters from URL  */
 /*Flight::map('query', function($username, $default_value = NULL){
