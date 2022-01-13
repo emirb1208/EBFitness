4fff1f2aabc74f655d789906590a2fd4a0fc0a13
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-13 10:43:40 +0100

    accounts.php update

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -9,21 +9,17 @@ Flight::route('GET /accounts', function(){
     });
 
 Flight::route('GET /accounts/@id', function($id){
-    Flight::json(Flight::accountDao()->get_by_id($id));
+    Flight::json(Flight::accountService()->get_by_id($id));
 });
 
 Flight::route('POST /accounts', function(){
     $data = Flight::request()->data->getData();
-    Flight::json(Flight::accountDao()->add($data));
-    //Flight::json(Flight::accountService()->add($data));
+    Flight::json(Flight::accountService()->add($data));
 });
 
 Flight::route('PUT /accounts/@id', function($id){
-    $request = Flight::request();
-    $data = $request->data->getData();
-    Flight::accountDao()->update($id, $data);
-    Flight::json(Flight::accountDao()->get_by_id($id));
-    //Flight::json(Flight::accountService()->update($id, $data));
+    $data = Flight::request()->data->getData();
+    Flight::json(Flight::accountService()->update($id, $data));
 });
 
 
