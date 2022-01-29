1ace7e98fd84d0c4e8aaab476554f96cfecaf738
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-01-29 13:43:58 +0100

    testing env variabales and change config file to read them, add db port

--- api/config.php
+++ api/config.php
@@ -2,18 +2,41 @@
 class Config {
 
   const DATE_FORMAT = "Y-m-d H:i:s";
-  const DB_HOST = "localhost";
-  const DB_USERNAME = "emirbeba12";
-  const DB_PASSWORD = "emiremir";
-  const DB_SCHEME = "ebfitness";
 
-  const SMTP_HOST = "smtp.mailgun.org";
-  const SMTP_PORT = 587;
-  const SMTP_USER = "postmaster@sandboxcda632bb57bb4a28a5eba2f44b8492e1.mailgun.org";
-  const SMTP_PASSWORD = ""; //55623b8eb3d8e0213afae544d5ed1679-ef80054a-bdfa3e44
+public static function DB_HOST(){
+return Config::get_env("DB_HOST", "localhost");
+}
+public static function DB_USERNAME(){
+  return Config::get_env("DB_USERNAME", "emirbeba12");
+}
+public static function DB_PASSWORD(){
+  return Config::get_env("DB_PASSWORD", "emiremir");
+}
+public static function DB_SCHEME(){
+  return Config::get_env("DB_SCHEME", "ebfitness");
+}
+public static function DB_PORT(){
+  return Config::get_env("DB_PORT", "3306");
+}
+public static function SMTP_HOST(){
+  return Config::get_env("SMTP_HOST", "smtp.mailgun.org");
+}
+public static function SMTP_PORT(){
+  return Config::get_env("SMTP_PORT", "587");
+}
+public static function SMTP_USER(){
+  return Config::get_env("SMTP_USER", NULL);
+}
+public static function SMTP_PASSWORD(){
+  return Config::get_env("SMTP_PASSWORD", NULL);
+}
 
   const JWT_SECRET ="m1qx2KcwtcZVqn3F7upF";
   const JWT_TOKEN_TIME = 2419200;
+
+  public static function get_env($name, $default){
+  return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+ }
 }
 
 ?>
