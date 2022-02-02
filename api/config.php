5fe9eb826a8d53a8a6c9740a670342fe6c567c58
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-02-02 21:29:05 +0100

    config php update

--- api/config.php
+++ api/config.php
@@ -3,20 +3,20 @@ class Config {
 
   const DATE_FORMAT = "Y-m-d H:i:s";
 
-  public static function DB_HOST(){
-  return Config::get_env("DB_HOST", "ebfitness-db-do-user-10688262-0.b.db.ondigitalocean.com");
-  }
-  public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "doadmin");
-  }
-  public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "bvLJOYNmBlPKGCgV");
-  }
-  public static function DB_SCHEME(){
-    return Config::get_env("DB_SCHEME", "ebfitness");
-  }
+public static function DB_HOST(){
+  return Config::get_env("DB_HOST", "localhost");
+}
+public static function DB_USERNAME(){
+  return Config::get_env("DB_USERNAME", "emirbeba12");
+}
+public static function DB_PASSWORD(){
+  return Config::get_env("DB_PASSWORD", "emiremir");
+}
+public static function DB_SCHEME(){
+  return Config::get_env("DB_SCHEME", "ebfitness");
+}
 public static function DB_PORT(){
-  return Config::get_env("DB_PORT", "25060");
+  return Config::get_env("DB_PORT", "3306");
 }
 public static function SMTP_HOST(){
   return Config::get_env("SMTP_HOST", "smtp.mailgun.org");
