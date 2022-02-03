205ac86f6440a1120100f32dd6ed0931b05d4628
emirb1208 <emir.beba@stu.ibu.edu.ba>
2022-02-03 12:16:30 +0100

    update config php

--- api/config.php
+++ api/config.php
@@ -4,16 +4,16 @@ class Config {
   const DATE_FORMAT = "Y-m-d H:i:s";
 
 public static function DB_HOST(){
-  return Config::get_env("DB_HOST", "remotemysql.com");
+  return Config::get_env("DB_HOST", "localhost");
 }
 public static function DB_USERNAME(){
-  return Config::get_env("DB_USERNAME", "Z7JYjYTfTv");
+  return Config::get_env("DB_USERNAME", "emirbeba12");
 }
 public static function DB_PASSWORD(){
-  return Config::get_env("DB_PASSWORD", "XrxAKOcw66");
+  return Config::get_env("DB_PASSWORD", "emiremir");
 }
 public static function DB_SCHEME(){
-  return Config::get_env("DB_SCHEME", "proba");
+  return Config::get_env("DB_SCHEME", "ebfitness");
 }
 public static function DB_PORT(){
   return Config::get_env("DB_PORT", "3306");
